c7810c667af43d535456d0cb15c1af94b38f2dcf
maximilian <enolpeaxlian120@gmail.com>
2026-02-14 09:15:28 +0800

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 
-  base: "https://maxxqcty.github.io/trial-and-error/",
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
